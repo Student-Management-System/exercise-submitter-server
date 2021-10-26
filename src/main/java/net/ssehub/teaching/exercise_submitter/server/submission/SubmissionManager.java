ede67fda196a0f09c5bd53412a3235e0d960424e
Adam <adam@sse.uni-hildesheim.de>
2021-10-26 19:44:24 +0200

    SubmissionManager: Fix deletion of temporary directory

--- src/main/java/net/ssehub/teaching/exercise_submitter/server/submission/SubmissionManager.java
+++ src/main/java/net/ssehub/teaching/exercise_submitter/server/submission/SubmissionManager.java
@@ -1,8 +1,11 @@
 package net.ssehub.teaching.exercise_submitter.server.submission;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -119,7 +122,18 @@ public class SubmissionManager {
         } finally {
             if (temporaryDirectory != null) {
                 try {
-                    Files.delete(temporaryDirectory);
+                    Files.walkFileTree(temporaryDirectory, new SimpleFileVisitor<Path>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            Files.delete(file);
+                            return FileVisitResult.CONTINUE;
+                        }
+                        @Override
+                        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                            Files.delete(dir);
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
                 } catch (IOException e) {
                     LOGGER.log(Level.WARNING, "Failed to delete temporary submission directory", e);
                 }
